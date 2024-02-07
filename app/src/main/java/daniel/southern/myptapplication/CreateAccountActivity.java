c20f5de94aba05daf2a72feb6f5aed7d3a5dbdc1
bh98cs <151757473+bh98cs@users.noreply.github.com>
2024-02-07 14:32:04 +0000

    Added code for logging in and creating new account using Firebase Auth.

--- app/src/main/java/daniel/southern/myptapplication/CreateAccountActivity.java
+++ app/src/main/java/daniel/southern/myptapplication/CreateAccountActivity.java
@@ -1,14 +1,146 @@
 package daniel.southern.myptapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //constant variable for sending contents of email address field to LoginActivity
+    public static final String EXTRA_EMAIL_ADDRESS = "daniel.southern.myptapplication.EXTRA_EMAIL_ADDRESS";
+    //tag for logs
+    public static final String TAG = "CreateAccountActivity";
+    //declare instance of FireBase Auth
+    private FirebaseAuth mAuth;
+    //declare instances of views
+    private EditText userEmail;
+    private EditText userPassword;
+    private Button createAccBtn;
+    private Button loginBtn;
+    private EditText confirmPassword;
+    private Toolbar toolbar;
+    private ImageView homepageIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        //set toolbar as activities actionbar
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //set on click listener for HomePage icon
+        homepageIcon = findViewById(R.id.imageView_homepageIcon);
+        homepageIcon.setOnClickListener(this);
+
+        //assign variables to EditText views and Buttons
+        userEmail = findViewById(R.id.editText_UserEmail);
+        userPassword = findViewById(R.id.editText_Password);
+        confirmPassword = findViewById(R.id.editText_confirmPassword);
+        createAccBtn = findViewById(R.id.button_CreateAccount);
+        loginBtn = findViewById(R.id.button_Login);
+
+        //initialise firebase auth
+        mAuth = FirebaseAuth.getInstance();
+
+        //set onClick listeners for buttons
+        loginBtn.setOnClickListener(this);
+        createAccBtn.setOnClickListener(this);
+
+        //get intent that started activity
+        Intent intent = getIntent();
+        //store any text from the intent in a local variable
+        String email = intent.getStringExtra(CreateAccountActivity.EXTRA_EMAIL_ADDRESS);
+        //prepopulate email address for user if filled out in LoginActivity
+        userEmail.setText(email);
     }
+    @Override
+    public void onClick(View v) {
+        //get user email outside of if statement as needed for both scenarios
+        String email = userEmail.getText().toString().trim();
+        //user clicks to create account
+        if(v.getId() == R.id.button_CreateAccount){
+            //try-catch block for getting string values from user input
+            try{
+                //store contents of the two password boxes in variables
+                String password = userPassword.getText().toString();
+                String password2 = confirmPassword.getText().toString();
+                //check if the two passwords match (to make sure user hasn't miss typed)
+                if(passwordsMatch(password, password2)){
+                    //create new user if passwords match
+                    createNewUser(email, password);
+                }
+                else {
+                    //user feedback to advise two password boxes do not match
+                    Toast.makeText(this, "Passwords do not match.", Toast.LENGTH_LONG).show();
+                }
+            }
+            //catch exception if null or empty string is provided
+            catch(IllegalArgumentException e){
+                //user feedback to advise all fields must be provided.
+                Toast.makeText(this, "Please provide an email address and two matching passwords.", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        //login button has been clicked
+        else if (v.getId() == R.id.button_Login) {
+            //send user to login page
+            Intent intent = new Intent(this, LoginActivity.class);
+            //send contents of email address box to login page
+            intent.putExtra(EXTRA_EMAIL_ADDRESS, email);
+            startActivity(intent);
+        }
+        //homepage icon clicked
+        else if (v.getId() == R.id.imageView_homepageIcon) {
+            //send user back to homepage
+            Intent intent = new Intent(this, HomePageActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    //create new user and save credentials to Firebase
+    private void createNewUser(String email, String password){
+        //create new user using email and password
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //check if task was successful
+                        if(task.isSuccessful()){
+                            //sign up success, go to main page
+                            Log.d(TAG, "createUserWithEmail:success");
+                            Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }else {
+                            //sign up fails print error in the log
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            //user feedback to advise could not create account
+                            Toast.makeText(CreateAccountActivity.this, "Unable to Create Account.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    //check if both password boxes are identical
+    private boolean passwordsMatch(String pw1, String pw2){
+        return pw1.equals(pw2);
+    }
+
 }
--- app/src/main/java/daniel/southern/myptapplication/LoginActivity.java
+++ app/src/main/java/daniel/southern/myptapplication/LoginActivity.java
@@ -1,14 +1,146 @@
 package daniel.southern.myptapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //tag for logs for this activity
+    public static final String TAG = "LoginActivity";
+    //for intenting email address to another activity
+    public static final String EXTRA_EMAIL_ADDRESS = "daniel.southern.myptapplication.EXTRA_EMAIL_ADDRESS";
+
+    //declare instance of firebase auth
+    private FirebaseAuth mAuth;
+
+    //declare functional views on Activity
+    private Button loginBtn;
+    private Button createAccBtn;
+    private EditText userEmail;
+    private EditText userPassword;
+    private Toolbar toolbar;
+    private ImageView homepageIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        //create toolbar
+        toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //hide title as custom one created in layout file
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //initialize homepage icon and set onclick listener
+        homepageIcon = findViewById(R.id.imageView_homepageIcon);
+        homepageIcon.setOnClickListener(this);
+
+        //Initialize views
+        loginBtn = findViewById(R.id.button_Login);
+        createAccBtn = findViewById(R.id.button_CreateAccount);
+        userEmail = findViewById(R.id.editText_UserEmail);
+        userPassword = findViewById(R.id.editText_Password);
+
+        //set onclick listener for buttons
+        loginBtn.setOnClickListener(this);
+        createAccBtn.setOnClickListener(this);
+
+        //get intent that started activity
+        Intent intent = getIntent();
+        //store any text from the intent in a local variable
+        String email = intent.getStringExtra(CreateAccountActivity.EXTRA_EMAIL_ADDRESS);
+        //prepopulate email address for user if filled out in CreateAccountActivity
+        userEmail.setText(email);
+
     }
+    @Override
+    public void onClick(View v) {
+        //login button clicked
+        if(v.getId() == R.id.button_Login){
+            //try-catch block for getting string values from user input
+            try{
+                //store user input in string variables
+                String email = userEmail.getText().toString().trim();
+                String password = userPassword.getText().toString().trim();
+
+                //call method to sign in user with email and password given
+                signIn(email, password);
+            }
+            //catch exception if null or empty string is provided
+            catch(IllegalArgumentException e){
+                //user feedback to advise both fields need to be given
+                Toast.makeText(LoginActivity.this, "Please provide an email address and password.", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+        //create account button clicked
+        else if (v.getId() == R.id.button_CreateAccount) {
+            //get contents of email input box
+            String email = userEmail.getText().toString().trim();
+            //send user to create account page
+            Intent intent = new Intent(this, CreateAccountActivity.class);
+            //send contents of email input to CreateAccountActivity
+            intent.putExtra(EXTRA_EMAIL_ADDRESS, email);
+            startActivity(intent);
+        } else if (v.getId() == R.id.imageView_homepageIcon) {
+            //send user to homepage
+            Intent intent = new Intent(this, HomePageActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void signIn(String email, String password) {
+
+        //sign user in with their email and password
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //check if login was successful
+                        if(task.isSuccessful()){
+                            //sign in successful
+                            Log.d(TAG, "signInWithEmail:success");
+                            //send user to their main page
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }else{
+                            //sign in unsuccessful
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Email or Password is incorrect.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //if attempt to sign in failed
+                        Log.e(TAG, "Unable to attempt sign in.", e);
+                        //user feedback
+                        Toast.makeText(LoginActivity.this, "Unable to Sign in.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
 }
