f882d24599ca111debf1d131527dbb8742dfae93
bh98cs <151757473+bh98cs@users.noreply.github.com>
2024-04-17 11:10:25 +0100

    comments added

--- app/src/main/java/daniel/southern/myptapplication/posedetector/classification/ClassificationResult.java
+++ app/src/main/java/daniel/southern/myptapplication/posedetector/classification/ClassificationResult.java
@@ -8,23 +8,38 @@ import java.util.Set;
 
 public class ClassificationResult {
 
-    // For an entry in this map, the key is the class name, and the value is how many times this class
-    // appears in the top K nearest neighbors. The value is in range [0, K] and could be a float after
-    // EMA smoothing. We use this number to represent the confidence of a pose being in this class.
+    //hash map to store confidence scores for exercises
     private final Map<String, Float> classConfidences;
 
+    /**
+     * ClassificationResult class constructor.
+     * Creates new hashmap to store classes and confidence scores
+     */
     public ClassificationResult() {
         classConfidences = new HashMap<>();
     }
 
+    /**
+     * Retrieves all classes.
+     * @return set view of exercise classes
+     */
     public Set<String> getAllClasses() {
         return classConfidences.keySet();
     }
 
+    /**
+     * Returns confidence score for the selected exercise class
+     * @param className name of the exercise class
+     * @return confidence score for the exercise class passed as a parameter
+     */
     public float getClassConfidence(String className) {
         return classConfidences.containsKey(className) ? classConfidences.get(className) : 0;
     }
 
+    /**
+     * Retrieves name of the exercise with the highest confidence score
+     * @return name of the exercise with the highest confidence score
+     */
     public String getMaxConfidenceClass() {
         return max(
                 classConfidences.entrySet(),
@@ -32,11 +47,20 @@ public class ClassificationResult {
                 .getKey();
     }
 
+    /**
+     * Increments the confidence score for the exercise class passed as a parameter
+     * @param className name of exercise class to increment confidence score for
+     */
     public void incrementClassConfidence(String className) {
         classConfidences.put(className,
                 classConfidences.containsKey(className) ? classConfidences.get(className) + 1 : 1);
     }
 
+    /**
+     * Adds exercise name and its confidence score to the classConfidences hash map
+     * @param className name of the exercise class
+     * @param confidence confidence score for the exercise
+     */
     public void putClassConfidence(String className, float confidence) {
         classConfidences.put(className, confidence);
     }
